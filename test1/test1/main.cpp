283a60683eef176c03e1cacba6cf980a9c29cead
jinjo <creationer625@outlook.com>
2022-10-26 16:27:31 +0800

    intial project

---
+++ test1/test1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test1
+//
+//  Created by ShuYang Yan on 2022/10/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
